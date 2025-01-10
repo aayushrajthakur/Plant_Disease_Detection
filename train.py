35511f5414381b0fc03cec019d5c8b7f94eac969
Aayush raj thakur <aayushrajthakur@gmail.com>
2025-01-10 19:10:13 +0530

    This project is almost completed but the training has done on very less data. so, The accuracy is very less. You can download the large data set from kaggle.com and retrain the model to get the more precise result.

--- train.py
+++ train.py
@@ -2,6 +2,9 @@ import tensorflow as tf
 from model import create_model
 from data_preprocessing import load_data
 
+
+
+
 def train_model(test, epochs=10, batch_size=32):
     images, labels = load_data(test)
     model = create_model()
